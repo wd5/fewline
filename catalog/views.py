4a2798f1c154375a0b766103ecb465643b2ee77e
Vladimir Pal <vladimir@MacBook-Pro-Vladimir.local>
2012-03-16 13:12:10 +0400

    Bug fix

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,4 @@
          # -*- coding: utf-8 -*-
-import re
-from django.db.models.query_utils import Q
 from django.shortcuts import get_object_or_404, render_to_response
 from django.core import urlresolvers
 from django.template import RequestContext
@@ -12,18 +10,11 @@ from django.core.mail import send_mail
 from cart import settings
 
 def index(request):
-    page_title = u'Монтаж видеонаблюдения, установка видеонаблюдения - Цептум. Москва'
-    meta_keywords = """установка систем видеонаблюдения монтаж камер лицензия сигнализация прайс
-     лист стоимость услуги в квартира офис школа помещение"""
-    meta_description = """Все для Видеонаблюдения - установка и монтаж систем видеонаблюдени.
-     Продажа видеокамер, видеорегистраторов и аксессуаров по доступным ценам"""
+    page_title = u'Fewline'
+    meta_keywords = """"""
+    meta_description = """"""
     sections = Section.objects.all()
     return render_to_response("main/index.html", locals(), context_instance=RequestContext(request))
-
-def cats(request):
-    cats = Category.objects.all()
-    sections = Section.objects.all()
-    return render_to_response("main/cats.html", locals(), context_instance=RequestContext(request))
 
 def show_category(request, category_slug):
     category = Category.objects.get(slug=category_slug)
@@ -34,9 +25,9 @@ def show_category(request, category_slug):
     else:
         products = category.product_set.filter(is_active=True).order_by('categoryproduct__position')
     if category.section.name == category.name:
-        page_title = u'%s - Цептум' % category.section
+        page_title = u'%s - Fewline' % category.section
     else:
-        page_title = u'%s %s - Цептум' % (category.section, category)
+        page_title = u'%s %s - Fewline' % (category.section, category)
     meta_keywords = category.meta_keywords
     meta_description = category.meta_descriotion
     meta_keywords = category.meta_keywords
@@ -60,28 +51,18 @@ def show_product(request, product_slug):
             cart.add_to_cart(request)
             url = urlresolvers.reverse('show_cart')
             return HttpResponseRedirect(url)
-    page_title = u'%s - Цептум' % product.name
+    page_title = u'%s - Fewline' % product.name
     meta_keywords = page_title
     meta_description = "%s - %s" % (page_title, product.mini_html_description)
     sections = Section.objects.all()
     return render_to_response("main/tovar.html", locals(), context_instance=RequestContext(request))
 
-def all_goods(request):
-    if request.method == 'POST':
-        cart.add_to_cart(request)
-        url = urlresolvers.reverse('show_cart')
-        return HttpResponseRedirect(url)
-    products = Product.objects.filter(is_active=True)
-    page_title = u'Цептум Все товары'
-    meta_keywords = page_title
-    return render_to_response("main/catalog.html", locals(), context_instance=RequestContext(request))
-
 def about(request):
-    page_title = u'О нас - Цептум'
+    page_title = u'О нас - Fewline'
     return render_to_response('main/about.html', locals(), context_instance=RequestContext(request))
 
 def delivery(request):
-    page_title = u'Оплата и Монтаж - Цептум'
+    page_title = u'Оплата и Монтаж - Fewline'
     return render_to_response('main/delivery.html', locals(), context_instance=RequestContext(request))
 
 def internal_error(request):
