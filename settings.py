cbada42fec0dfb08aaf1db3189259855fb9ccaee
Vladimir Pal <vladimir@MacBook-Pro-Vladimir.local>
2012-03-26 15:31:46 +0400

    Bug fix

--- settings.py
+++ settings.py
@@ -81,7 +81,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-#    'fewline.ReferMiddleware.ReferMiddleware',
+    'fewline.ReferMiddleware.ReferMiddleware',
 )
 
 ROOT_URLCONF = 'fewline.urls'
